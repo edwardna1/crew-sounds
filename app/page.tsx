90706629a86db0c96dafffd8890168d158b75fa6
edwardna1 <77164264+edwardna1@users.noreply.github.com>
2023-03-14 20:39:44 -0400

    testing this

--- app/page.tsx
+++ app/page.tsx
@@ -4,40 +4,50 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex h-screen bg-black">
-      <div className="w-screen h-screen flex flex-col justify-center items-center mb-100">
+      <div className="w-screen h-screen flex flex-col justify-center items-center">
         <Image
-          width={150}
-          height={150}
+          width={512}
+          height={512}
           src="/logo.png"
           alt="Platforms on Vercel"
-          className="w-52 h-52"
+          className="w-48 h-48"
         />
         <div className="text-center max-w-screen-sm mb-10">
-          <h1 className="text-stone-200 font-bold text-2xl">Crew Sound</h1>
+          <h1 className="text-stone-200 font-bold text-2xl">
+            Next.js Prisma MySQL Auth Starter
+          </h1>
           <p className="text-stone-400 mt-5">
-            This is an app to look at your Spotify compatibility with users, in
-            real time.
+            This is a{" "}
+            <a
+              href="https://nextjs.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-stone-400 underline hover:text-stone-200 transition-all"
+            >
+              Next.js
+            </a>{" "}
+            starter kit that uses{" "}
+            <a
+              href="https://next-auth.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-stone-400 underline hover:text-stone-200 transition-all"
+            >
+              Next-Auth
+            </a>{" "}
+            for simple email + password login and a MySQL database to persist
+            the data.
           </p>
         </div>
         <div className="flex space-x-3">
-          {/* <Link
+          <Link
             href="/protected"
             prefetch={false} // workaround until https://github.com/vercel/vercel/pull/8978 is deployed
             className="text-stone-400 underline hover:text-stone-200 transition-all"
           >
             Protected Page
-          </Link> */}
-          <button
-            className="border-black bg-green-600 py-5 px-8 text-black font-semibold  hover:bg-green-400 flex h-10 w-full items-center justify-center rounded-md border"
-          >
-            <Link
-              href="/protected"
-              prefetch={false} // workaround until https://github.com/vercel/vercel/pull/8978 is deployed
-            >
-              Log in
-            </Link>
-          </button>
-          {/* <p className="text-white space">·</p>
+          </Link>
+          <p className="text-white">·</p>
           <a
             href="https://github.com/steven-tey/nextjs-typescript-starter"
             target="_blank"
@@ -45,7 +55,7 @@ export default function Home() {
             className="text-stone-400 underline hover:text-stone-200 transition-all"
           >
             GitHub
-          </a> */}
+          </a>
         </div>
       </div>
     </div>
