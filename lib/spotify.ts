dec3d5e051c08b702ec591e49707e5605a6ac7ae
edwardna1 <77164264+edwardna1@users.noreply.github.com>
2023-03-17 21:13:12 -0400

    added some next auth

---
+++ lib/spotify.ts
@@ -0,0 +1,32 @@
+import SpotifyWebApi from "spotify-web-api-node";
+
+const scopes = [
+  "user-read-email",
+  "user-read-private",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+  "playlist-modify-private",
+  "playlist-modify-public",
+  "user-library-modify",
+  "user-library-read",
+  "user-read-playback-position",
+  "user-top-read",
+  "user-read-recently-played",
+].join(", ");
+
+const params = {
+  scope: scopes,
+};
+
+const queryParamString = new URLSearchParams(params);
+
+const LOGIN_URL = `https://accounts.spotify.com/authorize${queryParamString.toString()}`;
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.NEXT_PUBLIC_CLIENT_ID ?? "",
+  clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET ?? "",
+});
+
+export default spotifyApi;
+
+export { LOGIN_URL };
